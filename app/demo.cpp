0f26772f96ca6406e22b0c4e2f65f793e95c11ef
Christof Paessler <christof.paessler@gmail.com>
2025-10-06 19:26:50 +0200

    added command line arguments

--- app/demo.cpp
+++ app/demo.cpp
@@ -2,8 +2,17 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc < 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <address> <port>" << std::endl;
+        return 1;
+    }
+    
+    std::string address = argv[1];
+    int port = std::stoi(argv[2]);
+
     Server server;
-    server.start();
+    server.start(address, port);
     
     return 0;
 }
